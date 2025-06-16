0df9085e376d0a286684cb390d9128d5f9c62b6d
luisevb24 <luisevb.5s@gmail.com>
2025-06-16 15:11:18 -0600

    Style the book list, the page header and the new book button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 const booksList = document.querySelector('.books');
-
+const newBookBtn = document.querySelector('.newBook');
+const newBookDialog = document.querySelector('.newBookModal');
 
 function Book(title, author, numOfPages, isRead, id){
     if(!new.target) throw Error;
@@ -20,4 +21,34 @@ function addBookToMyLibrary(title, author, numOfPages, isRead){
 addBookToMyLibrary('Lord of Chaos', 'Robert Jordan', 800, true);
 addBookToMyLibrary('Siddharta', 'Hermann Hesse', 200, false);
 
-console.log(myLibrary);
+myLibrary.forEach((book) => {
+    let bookCard = document.createElement('li');
+    bookCard.classList.add('bookCard');
+
+
+    let bookTitle = document.createElement('div');
+    bookTitle.classList.add('bookTitle');
+    bookTitle.textContent = `${book.title}`;
+
+    let bookAuthor = document.createElement('div');
+    bookAuthor.classList.add('bookAuthor');
+    bookAuthor.textContent = `by ${book.author}`;
+
+    let bookInfo = document.createElement('div');
+    bookInfo.classList.add('bookPages');
+    bookInfo.textContent = `Number of Pages: ${book.numOfPages}`
+
+    let bookRead = document.createElement('div');
+    if (book.isRead) {
+        bookRead.classList.add('bookRead');
+    } else {
+        bookRead.classList.add('bookNotRead');
+    }
+
+    booksList.appendChild(bookCard);
+    bookCard.append(bookTitle, bookAuthor, bookInfo, bookRead);
+});
+
+newBookBtn.addEventListener('click', () => {
+    newBookDialog.showModal();
+})
