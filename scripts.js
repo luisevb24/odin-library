af3b63df648d0fc7532255b2c246912495b301a4
luisevb24 <luisevb.5s@gmail.com>
2025-06-17 08:33:53 -0600

    Add remove book button, style it and make it work"

--- scripts.js
+++ scripts.js
@@ -3,9 +3,22 @@ const booksList = document.querySelector('.books');
 const newBookBtn = document.querySelector('.newBook');
 const newBookDialog = document.querySelector('.newBookModal');
 const closeDialogBtn = document.querySelector('.closeDialog');
+const newBookForm = document.querySelector('.newBookForm');
+const removeBtn = document.querySelector('.removeBtn');
 
-function Book(title, author, numOfPages, isRead, id){
-    if(!new.target) throw Error;
+newBookForm.addEventListener('submit', () => {
+    let bookData = new FormData(newBookForm);
+    let title = bookData.get('book-title');
+    let author = bookData.get('book-author');
+    let pages = bookData.get('book-pages');
+    let isRead = bookData.get('book-read-state');
+
+    addBookToMyLibrary(title, author, pages, isRead);
+}
+)
+
+function Book(title, author, numOfPages, isRead, id) {
+    if (!new.target) throw Error;
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
@@ -13,42 +26,63 @@ function Book(title, author, numOfPages, isRead, id){
     this.id = id;
 }
 
-function addBookToMyLibrary(title, author, numOfPages, isRead){
-    let id = self.crypto.randomUUID();
-    let newBook = new Book(title, author, numOfPages, isRead, id);
-    myLibrary.push(newBook);
+Book.prototype.toggleIsRead = function () {
+    this.isRead = !this.isRead;
 }
 
-addBookToMyLibrary('Lord of Chaos', 'Robert Jordan', 800, true);
-addBookToMyLibrary('Siddharta', 'Hermann Hesse', 200, false);
-
-myLibrary.forEach((book) => {
-    let bookCard = document.createElement('li');
+function renderBookCard(book) {
+    const bookCard = document.createElement('li');
     bookCard.classList.add('bookCard');
 
-
-    let bookTitle = document.createElement('div');
+    const bookTitle = document.createElement('div');
     bookTitle.classList.add('bookTitle');
     bookTitle.textContent = `${book.title}`;
 
-    let bookAuthor = document.createElement('div');
+    const bookAuthor = document.createElement('div');
     bookAuthor.classList.add('bookAuthor');
     bookAuthor.textContent = `by ${book.author}`;
 
-    let bookInfo = document.createElement('div');
+    const bookInfo = document.createElement('div');
     bookInfo.classList.add('bookPages');
-    bookInfo.textContent = `Number of Pages: ${book.numOfPages}`
+    bookInfo.textContent = `Number of Pages: ${book.numOfPages}`;
 
-    let bookRead = document.createElement('div');
+    const bookRead = document.createElement('div');
     if (book.isRead) {
         bookRead.classList.add('bookRead');
     } else {
         bookRead.classList.add('bookNotRead');
     }
 
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('removeBtn');
+    removeBtn.textContent = '';
+
+
+    bookCard.setAttribute('data-id', book.id);
     booksList.appendChild(bookCard);
-    bookCard.append(bookTitle, bookAuthor, bookInfo, bookRead);
+    bookCard.append(bookTitle, bookAuthor, bookInfo, bookRead, removeBtn);
+
+    removeBtn.addEventListener('click', (event) => {
+        const btn = event.target;
+        const bookCard = btn.parentElement;
+        const bookId = bookCard.dataset.id;
+        removeBook(bookId);
+        bookCard.remove();
+        console.log(myLibrary);
+    });
+}
+
+function addBookToMyLibrary(title, author, numOfPages, isRead) {
+    let id = self.crypto.randomUUID();
+    let newBook = new Book(title, author, numOfPages, isRead, id);
+    myLibrary.push(newBook);
+    renderBookCard(newBook);
+}
+
+myLibrary.forEach((book) => {
+    renderBookCard(book);
 });
+
 
 newBookBtn.addEventListener('click', () => {
     newBookDialog.showModal();
@@ -57,3 +91,15 @@ newBookBtn.addEventListener('click', () => {
 closeDialogBtn.addEventListener('click', () => {
     newBookDialog.close();
 })
+
+
+
+addBookToMyLibrary('The Dragon Reborn', 'Robert Jordan', 650, true);
+addBookToMyLibrary('Siddharta', 'Hermann Hesse', 200, false);
+
+function removeBook(bookId) {
+    const bookIndex = myLibrary.findIndex((book) => book.id == bookId);
+    if (bookIndex !== -1) {
+        myLibrary.splice(bookIndex, 1);
+    }
+}
